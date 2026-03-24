31d499d5e7df6816d0e41dfd196b247c9734ad7c
Kshitij Chandrakar <kshitijchandrakar2019@gmail.com>
2026-03-24 13:54:46 +0530

    Overhawl 1

--- ui/urls.py
+++ ui/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from . import Excel, Export, views
+# from . import Excel, Export,
+from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -10,7 +11,7 @@ urlpatterns = [
     path("api/category", views.get_categories_json, name="get_categories"),
     path("api/transaction-add", views.transaction_add, name="transaction_add"),
     path("api/transactions/", views.transaction_api, name="transaction_api"),
-    path("api/typst-json/", Export.export_to_json, name="export_to_json"),
-    path("api/excel-export/", Export.create_excel_response, name="excel_export"),
-    path("api/excel-import/", Export.upload_excel, name="excel_import"),
+    # path("api/typst-json/", Export.export_to_json, name="export_to_json"),
+    # path("api/excel-export/", Export.create_excel_response, name="excel_export"),
+    # path("api/excel-import/", Export.upload_excel, name="excel_import"),
 ]
